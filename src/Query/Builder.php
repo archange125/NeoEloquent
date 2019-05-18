4f650e28c54ca6db6e80ba455ca32c95b3eea36d
Junior Ngangeli <juniorngangeli3@gmail.com>
2019-05-18 10:25:51 +0100

    Fixed Builder@first method offset check!

--- src/Query/Builder.php
+++ src/Query/Builder.php
@@ -751,8 +751,8 @@ class Builder extends IlluminateQueryBuilder {
      */
     public function first($columns = array('*'))
     {
-        $results = $this->take(1)->get($columns)->current();
-        //$results[0]) && count($results[0]) > 0
+        $resultSet = $this->take(1)->get($columns);
+        $results = $resultSet->count() > 0 ? $resultSet->current() : [];
         return (isset($results[0])) ? $results[0]->getProperties() : null;
     }
 
